3dcf2cbf8f6c25557fed1bc9a079fe94b06af4a0
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-02 19:53:27 +0300

    made combine_files function

--- combine_files.py
+++ combine_files.py
@@ -1,35 +1,27 @@
 from os import listdir
 from pathlib import Path
+from typing import List
 from natsort import natsorted
+
+
+def combine_files(filenames: List, output_filename: str):
+    texts = []
+    for filename in filenames:
+        with open(input_dir + filename, 'r', encoding='utf-8') as inf:
+            texts.append(inf.readlines())
+    lines_number = len(texts[0])
+    with open(output_dir + output_filename, 'w', encoding='utf-8') as ouf:
+        for line in range(lines_number):
+            for text in texts:
+                ouf.write(f'{text[line].strip()}\t')
+            ouf.write('\n')
+
 
 input_dir = './input/'
 output_dir = './output/'
 filenames = natsorted([f for f in listdir(input_dir) if Path(input_dir + f).is_file()])
-
-
-def write_header(begin, end, step):
-    for text in texts[begin:end:step]:
-        ouf.write(f'{text[0].strip()}\t')
-    ouf.write('\n')
-
-
-def write_lines(begin, end, step):
-    for line in range(1, lines_number):
-        for text in texts[begin:end:step]:
-            ouf.write(f'{text[line].strip()}\t')
-        ouf.write('\n')
-
-
-texts = []
-for filename in filenames:
-    with open(input_dir + filename, 'r', encoding='utf-8') as inf:
-        texts.append(inf.readlines())
-lines_number = len(texts[0])
-
+filenames1 = filenames[::2]
+filenames2 = filenames[1::2]
 Path(output_dir).mkdir(parents=True, exist_ok=True)
-with open(output_dir + 'field1.txt', 'w', encoding='utf-8') as ouf:
-    write_header(0, len(texts), 2)
-    write_lines(0, len(texts), 2)
-with open(output_dir + 'field2.txt', 'w', encoding='utf-8') as ouf:
-    write_header(1, len(texts), 2)
-    write_lines(1, len(texts), 2)
+combine_files(filenames1, 'field1.txt')
+combine_files(filenames2, 'field2.txt')
