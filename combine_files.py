7fa8ffd85b997ce3f0666859845362b1ba5344a5
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2021-11-19 22:04:47 +0300

    Combine all files in one added. Support of different lines number in each file added. Menu added.

--- combine_files.py
+++ combine_files.py
@@ -5,26 +5,46 @@ from typing import List
 from natsort import natsorted
 
 
-def combine_files(filenames: List, output_filename: str):
+def combine_files(files: List, output_file: str):
     texts = []
-    for filename in filenames:
-        with open(input_dir + filename, 'r', encoding='utf-8') as inf:
+    for file in files:
+        with open(file, 'r', encoding='utf-8') as inf:
             texts.append(inf.readlines())
-    lines_number = len(texts[0])
-    with open(output_dir + output_filename, 'w', encoding='utf-8') as ouf:
-        for line in range(lines_number):
+    lines_number = max(len(text) for text in texts)
+    with open(output_file, 'w', encoding='utf-8') as ouf:
+        for line_number in range(lines_number):
             for text in texts:
-                ouf.write(f'{text[line].strip()}\t')
+                if line_number < len(text):
+                    ouf.write(f'{text[line_number].strip()}\t')
+                else:
+                    ouf.write(f'\t')
             ouf.write('\n')
 
 
-input_dir = './detrended/'
-output_dir = './output/'
-filenames = natsorted([f for f in listdir(input_dir) if Path(input_dir + f).is_file()])
-filenames1 = filenames[::2]
-filenames2 = filenames[1::2]
-Path(output_dir).mkdir(parents=True, exist_ok=True)
-combine_files(filenames1, 'field1.txt')
-combine_files(filenames2, 'field2.txt')
-print(sys.version)
-print('Files combine done successfully!')
+def main():
+
+    choice = input('1: Combine all files\n2: Combine odd and even files separately\n3: Cancel\nEnter your choice: ')
+    if choice not in ['1', '2']:
+        print('Combine canceled')
+        return
+
+    input_dir = './detrended/'
+    output_dir = './output/'
+    files = natsorted([input_dir + f for f in listdir(input_dir) if Path(input_dir + f).is_file()])
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    for f in Path(output_dir).glob('*.*'):
+        f.unlink()
+    if choice == '1':
+        combine_files(files, output_dir + 'out.txt')
+    else:
+        files1 = files[::2]
+        files2 = files[1::2]
+        combine_files(files1, output_dir + 'out1.txt')
+        combine_files(files2, output_dir + 'out2.txt')
+
+    print(sys.version)
+    print('Files combined successfully!')
+
+
+if __name__ == '__main__':
+    main()
