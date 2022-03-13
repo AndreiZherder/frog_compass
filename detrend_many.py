a775b8619d07faca7e71ec2604442d11b2cab857
Andrei Zherder <88932094+AndreiZherder@users.noreply.github.com>
2022-03-13 10:03:39 +0300

    enhance

--- detrend_many.py
+++ detrend_many.py
@@ -1,12 +1,11 @@
 import sys
-from pathlib import Path
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
 
+
 def main():
-    filename = './tmp/1.txt'
-    Path(filename[:-4] + '.out.txt').unlink(missing_ok=True)
+    filename = 'data/1.txt'
 
     with open(filename, 'r', encoding='utf-8') as inf:
         reader = csv.reader(inf, dialect='excel-tab')
@@ -19,16 +18,16 @@ def main():
             for i in range(n):
                 if rows[i][j]:
                     vs[j].append(float(rows[i][j].replace(',', '.')))
-    v = np.array(vs[10])
+    v = np.array(vs[6])
     n = len(v)
     t = np.linspace(0, (n - 1), n)
-    x = np.concatenate([t[:200], t[n - 200:]])
-    y = np.concatenate([v[:200], v[n - 200:]])
+    x = np.concatenate([t[:450], t[n - 450:]])
+    y = np.concatenate([v[:450], v[n - 450:]])
     s = np.polynomial.polynomial.Polynomial.fit(x, y, 1).convert().coef
     k, b = s[1], s[0]
     trend = k * t + b
     v_detrended = v - trend
-    with open(filename[:-4] + '.out.txt', 'w', encoding='utf-8') as ouf:
+    with open(filename[:-4] + '_det.txt', 'w', encoding='utf-8') as ouf:
         for num in v_detrended:
             ouf.write(f'{num:.8f}\n')
     print(sys.version)
